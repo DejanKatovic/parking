fd4277ec8e8ecbe1278a667a8891b0f20ec6c40f
DejanKatovic <dejankatovic@gmail.com>
2021-08-11 01:12:07 -0400

    middleware files

--- middleware/ip_middleware.js
+++ middleware/ip_middleware.js
@@ -3,10 +3,24 @@ const requestIp = require('request-ip');
 const ipMiddleware = (req, res, next) => {
     const clientIp = requestIp.getClientIp(req);
     const now = Date.now();
-    console.log(now);
+    const one_log = user_log.filter(({ip}) => ip === clientIp);
+    console.log(one_log);
+    if (one_log.length >= process.env.LIMIT_REQUEST - 1) {
+        console.log((now - one_log[0].createdAt));
+        if ((now - one_log[0].createdAt) < process.env.LIMIT_DURATION) {
+            const result = {
+                success: false,
+                msg: 'You made too many requests in short duration',
+            }
+            res.json(result);
+            user_log.splice(user_log.indexOf(one_log[0]), 1);
+            user_log.push({ip: clientIp, createdAt: now});
+            return;
+        }
+        user_log.splice(user_log.indexOf(one_log[0]), 1);
+    }
+    user_log.push({ip: clientIp, createdAt: now});
 
-    req.clientIp = clientIp;
-    console.log(clientIp);
     next();
 }
 
